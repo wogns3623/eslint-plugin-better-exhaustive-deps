72903c1206c09d8087e6fcd44efd48fbcb91b9d9
Jaehun Kim <wogns3623@gmail.com>
2023-11-14 10:09:39 +0900

    Setup eslint, jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,25 @@
-module.exports = {
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+const config = {
   extends: ["eslint:recommended", "plugin:eslint-plugin/recommended"],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    requireConfigFile: false,
+  },
+  env: {
+    node: true,
+    jest: true,
+    es6: true,
+  },
+  plugins: ["no-for-of-loops"],
+  rules: {
+    "no-unused-vars": "off",
+    "no-case-declarations": "off",
+    "eslint-plugin/no-deprecated-context-methods": "warn",
+    "eslint-plugin/require-meta-has-suggestions": "off",
+  },
 };
+
+module.exports = config;
