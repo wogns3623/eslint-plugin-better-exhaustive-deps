403dfaee38e7630a54a80fa9ec98bd6ff02d82bd
Jaehun Kim <wogns3623@gmail.com>
2025-02-23 01:56:03 +0900

    Update version

--- test/ExhaustiveDeps.test.js
+++ test/ExhaustiveDeps.test.js
@@ -8,7 +8,7 @@
 "use strict";
 
 const ESLintTester = require("eslint").RuleTester;
-const ReactHooksESLintPlugin = require("../lib");
+const ReactHooksESLintPlugin = require("../lib/index.js");
 const ReactHooksESLintRule = ReactHooksESLintPlugin.rules["exhaustive-deps"];
 
 /**
