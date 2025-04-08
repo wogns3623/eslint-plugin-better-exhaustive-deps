03b82a1a2534c334661958b79893320fef715068
Jaehun Kim <wogns3623@gmail.com>
2025-04-08 21:55:39 +0900

    Add staticHooks option test

--- __tests__/BetterExhaustiveDeps-test.js
+++ __tests__/BetterExhaustiveDeps-test.js
@@ -30,7 +30,7 @@ function normalizeIndent(strings) {
  */
 
 /** @type {RuleTesterInput} */
-const tests = {
+const testCheckMemoizedVariableIsStatics = {
   valid: [
     {
       code: normalizeIndent`
@@ -274,13 +274,233 @@ const tests = {
   ],
 };
 
+/** @type {RuleTesterInput} */
+const testStaticHooks = {
+  valid: [
+    /** no matter what the staticValue is, it will be static if the staticHooks option is settled */
+    {
+      code: normalizeIndent`
+        function useStaticHook() {
+          const staticValue = { a:1 };
+          return staticValue;
+        }
+
+        function MyComponent() {
+          const staticValue = useStaticHook();
+          useEffect(() => {
+            console.log(staticValue);
+          }, []);
+        }
+    `,
+      options: [{ staticHooks: { useStaticHook: true } }],
+    },
+    /** can handle renamed return */
+    {
+      code: normalizeIndent`
+        function useStaticHook() {
+          const staticValue = { a:1 };
+          return staticValue;
+        }
+
+        function MyComponent() {
+          const sv = useStaticHook();
+          useEffect(() => {
+            console.log(sv);
+          }, []);
+        }
+    `,
+      options: [{ staticHooks: { useStaticHook: true } }],
+    },
+    /**
+     * can handle renamed hook
+     * @deprecated not supported yet
+     */
+    // {
+    //   code: normalizeIndent`
+    //     function useStaticHook() {
+    //       const staticValue = { a:1 };
+    //       return staticValue;
+    //     }
+
+    //     const useSh = useStaticHook;
+
+    //     function MyComponent() {
+    //       const sv = useSh();
+    //       useEffect(() => {
+    //         console.log(sv);
+    //       }, []);
+    //     }
+    // `,
+    //   options: [{ staticHooks: { useStaticHook: true } }],
+    //   // skip: true, // not in eslint RuleTester, but in @typescript-eslint/rule-tester RuleTester
+    // },
+    /** can handle destructured array return */
+    {
+      code: normalizeIndent`
+        function useHook() {
+          const staticValue = { a:1 };
+          const staticCallback = () => {};
+          return [staticValue, staticCallback];
+        }
+
+        function MyComponent() {
+          const [staticValue, staticCallback] = useHook();
+          useEffect(() => {
+            console.log(staticValue);
+            staticCallback();
+          }, []);
+        }
+    `,
+      options: [{ staticHooks: { useHook: [true, true] } }],
+    },
+    /** can handle destructured array return with renamed variables */
+    {
+      code: normalizeIndent`
+        function useHook() {
+          const staticValue = { a:1 };
+          const staticCallback = () => {};
+          return [staticValue, staticCallback];
+        }
+
+        function MyComponent() {
+          const [sv, sc] = useHook();
+          useEffect(() => {
+            console.log(sv);
+            sc();
+          }, []);
+        }
+    `,
+      options: [{ staticHooks: { useHook: [true, true] } }],
+    },
+    /** can handle destructured object return */
+    {
+      code: normalizeIndent`
+        function useHook() {
+          const staticValue = { a:1 };
+          const staticCallback = () => {};
+          return { staticValue, staticCallback };
+        }
+
+        function MyComponent() {
+          const { staticValue, staticCallback } = useHook();
+          useEffect(() => {
+            console.log(staticValue);
+            staticCallback();
+          }, []);
+        }
+    `,
+      options: [
+        {
+          staticHooks: { useHook: { staticValue: true, staticCallback: true } },
+        },
+      ],
+    },
+    /** can handle destructured object return with renamed variables */
+    {
+      code: normalizeIndent`
+        function useHook() {
+          const staticValue = { a:1 };
+          const staticCallback = () => {};
+          return { staticValue, staticCallback };
+        }
+
+        function MyComponent() {
+          const { staticValue: sv, staticCallback: sc } = useHook();
+          useEffect(() => {
+            console.log(sv);
+            sc();
+          }, []);
+        }
+    `,
+      options: [
+        {
+          staticHooks: { useHook: { staticValue: true, staticCallback: true } },
+        },
+      ],
+    },
+    /**
+     * can handle deep nested object return
+     * @deprecated not supported yet
+     */
+    // {
+    //   code: normalizeIndent`
+    //     function useHook() {
+    //       const value = { foo:{ staticProperty:1 } };
+    //       const staticCallback = () => {};
+    //       return { value, staticCallback };
+    //     }
+
+    //     function MyComponent() {
+    //       const { value, staticCallback: sc } = useHook();
+    //       useEffect(() => {
+    //         console.log(value?.foo?.staticProperty);
+    //         sc();
+    //       }, []);
+    //     }
+    // `,
+    //   options: [
+    //     {
+    //       staticHooks: {
+    //         useHook: {
+    //           value: { foo: { staticProperty: true } },
+    //           staticCallback: true,
+    //         },
+    //       },
+    //     },
+    //   ],
+    //   // skip: true,
+    // },
+  ],
+  invalid: [
+    // only works with staticHooks option
+    {
+      code: normalizeIndent`
+        function useHook() {
+          const value = { a:1 };
+          return value;
+        }
+        function MyComponent() {
+          const value = useHook();
+          useEffect(() => {
+            console.log(value);
+          }, []);
+        }
+    `,
+      errors: [
+        {
+          message:
+            "React Hook useEffect has a missing dependency: 'value'. " +
+            "Either include it or remove the dependency array.",
+          suggestions: [
+            {
+              desc: "Update the dependencies array to be: [value]",
+              output: normalizeIndent`
+                function useHook() {
+                  const value = { a:1 };
+                  return value;
+                }
+                function MyComponent() {
+                  const value = useHook();
+                  useEffect(() => {
+                    console.log(value);
+                  }, [value]);
+                }
+              `,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
 // For easier local testing
 if (!process.env.CI) {
   let only = [];
   let skipped = [];
   [
-    ...tests.valid,
-    ...tests.invalid,
+    ...testCheckMemoizedVariableIsStatics.valid,
+    ...testCheckMemoizedVariableIsStatics.invalid,
     // ...testsFlow.valid,
     // ...testsFlow.invalid,
     // ...testsTypescript.valid,
@@ -306,8 +526,10 @@ if (!process.env.CI) {
     }
     return true;
   };
-  tests.valid = tests.valid.filter(predicate);
-  tests.invalid = tests.invalid.filter(predicate);
+  testCheckMemoizedVariableIsStatics.valid =
+    testCheckMemoizedVariableIsStatics.valid.filter(predicate);
+  testCheckMemoizedVariableIsStatics.invalid =
+    testCheckMemoizedVariableIsStatics.invalid.filter(predicate);
   // testsFlow.valid = testsFlow.valid.filter(predicate);
   // testsFlow.invalid = testsFlow.invalid.filter(predicate);
   // testsTypescript.valid = testsTypescript.valid.filter(predicate);
@@ -318,25 +540,25 @@ if (!process.env.CI) {
   //   testsTypescriptEslintParserV4.invalid.filter(predicate);
 }
 
-describe("checkMemoizedVariableIsStatic", () => {
-  const parserOptionsV7 = {
+const parserOptionsV7 = {
+  ecmaFeatures: {
+    jsx: true,
+  },
+  ecmaVersion: 6,
+  sourceType: "module",
+};
+
+const languageOptionsV9 = {
+  ecmaVersion: 6,
+  sourceType: "module",
+  parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 6,
-    sourceType: "module",
-  };
+  },
+};
 
-  const languageOptionsV9 = {
-    ecmaVersion: 6,
-    sourceType: "module",
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-  };
-
+describe("better-exhaustive-deps/checkMemoizedVariableIsStatic", () => {
   // new ESLintTester({
   //   parser: require.resolve("@babel/eslint-parser"),
   //   parserOptions,
@@ -351,10 +573,7 @@ describe("checkMemoizedVariableIsStatic", () => {
   //   tests
   // );
 
-  const testsBabelEslint = {
-    valid: [...tests.valid],
-    invalid: [...tests.invalid],
-  };
+  const testsBabelEslint = testCheckMemoizedVariableIsStatics;
 
   // new ESLintTesterV7({
   //   parser: require.resolve("babel-eslint"),
@@ -376,10 +595,140 @@ describe("checkMemoizedVariableIsStatic", () => {
     testsBabelEslint
   );
 
-  const testsTypescriptEslintParser = {
-    valid: [...tests.valid],
-    invalid: [...tests.invalid],
-  };
+  const testsTypescriptEslintParser = testCheckMemoizedVariableIsStatics;
+
+  new ESLintTesterV7({
+    parser: require.resolve("@typescript-eslint/parser-v2"),
+    parserOptions: parserOptionsV7,
+  }).run(
+    "eslint: v7, parser: @typescript-eslint/parser@2.x",
+    ReactHooksESLintRule,
+    testsTypescriptEslintParser
+  );
+
+  new ESLintTesterV9({
+    languageOptions: {
+      ...languageOptionsV9,
+      parser: require("@typescript-eslint/parser-v2"),
+    },
+  }).run(
+    "eslint: v9, parser: @typescript-eslint/parser@2.x",
+    ReactHooksESLintRule,
+    testsTypescriptEslintParser
+  );
+
+  // // TypeError: Class extends value undefined is not a constructor or null
+  // new ESLintTesterV7({
+  //   parser: require.resolve("@typescript-eslint/parser-v3"),
+  //   parserOptions: parserOptionsV7,
+  // }).run(
+  //   "eslint: v7, parser: @typescript-eslint/parser@3.x",
+  //   ReactHooksESLintRule,
+  //   testsTypescriptEslintParser
+  // );
+
+  // new ESLintTesterV9({
+  //   languageOptions: {
+  //     ...languageOptionsV9,
+  //     parser: require("@typescript-eslint/parser-v3"),
+  //   },
+  // }).run(
+  //   "eslint: v9, parser: @typescript-eslint/parser@3.x",
+  //   ReactHooksESLintRule,
+  //   testsTypescriptEslintParser
+  // );
+
+  new ESLintTesterV7({
+    parser: require.resolve("@typescript-eslint/parser-v4"),
+    parserOptions: parserOptionsV7,
+  }).run(
+    "eslint: v7, parser: @typescript-eslint/parser@4.x",
+    ReactHooksESLintRule,
+    {
+      valid: [...testsTypescriptEslintParser.valid],
+      invalid: [...testsTypescriptEslintParser.invalid],
+    }
+  );
+
+  new ESLintTesterV9({
+    languageOptions: {
+      ...languageOptionsV9,
+      parser: require("@typescript-eslint/parser-v4"),
+    },
+  }).run(
+    "eslint: v9, parser: @typescript-eslint/parser@4.x",
+    ReactHooksESLintRule,
+    {
+      valid: [...testsTypescriptEslintParser.valid],
+      invalid: [...testsTypescriptEslintParser.invalid],
+    }
+  );
+
+  new ESLintTesterV7({
+    parser: require.resolve("@typescript-eslint/parser-v5"),
+    parserOptions: parserOptionsV7,
+  }).run(
+    "eslint: v7, parser: @typescript-eslint/parser@^5.0.0-0",
+    ReactHooksESLintRule,
+    {
+      valid: [...testsTypescriptEslintParser.valid],
+      invalid: [...testsTypescriptEslintParser.invalid],
+    }
+  );
+
+  new ESLintTesterV9({
+    languageOptions: {
+      ...languageOptionsV9,
+      parser: require("@typescript-eslint/parser-v5"),
+    },
+  }).run(
+    "eslint: v9, parser: @typescript-eslint/parser@^5.0.0",
+    ReactHooksESLintRule,
+    {
+      valid: [...testsTypescriptEslintParser.valid],
+      invalid: [...testsTypescriptEslintParser.invalid],
+    }
+  );
+});
+
+describe("better-exhaustive-deps/staticHooks", () => {
+  // new ESLintTester({
+  //   parser: require.resolve("@babel/eslint-parser"),
+  //   parserOptions,
+  // }).run("parser: @babel/eslint-parser static", ReactHooksESLintRule, tests);
+
+  // new ESLintTester({
+  //   parser: require.resolve("@typescript-eslint/parser-v5"),
+  //   parserOptions,
+  // }).run(
+  //   "parser: @typescript-eslint/parser@^5.0.0-0 static",
+  //   ReactHooksESLintRule,
+  //   tests
+  // );
+
+  const testsBabelEslint = testStaticHooks;
+
+  // new ESLintTesterV7({
+  //   parser: require.resolve("babel-eslint"),
+  //   parserOptions: parserOptionsV7,
+  // }).run(
+  //   "eslint: v7, parser: babel-eslint",
+  //   ReactHooksESLintRule,
+  //   testsBabelEslint
+  // );
+
+  new ESLintTesterV9({
+    languageOptions: {
+      ...languageOptionsV9,
+      parser: require("@babel/eslint-parser"),
+    },
+  }).run(
+    "eslint: v9, parser: @babel/eslint-parser",
+    ReactHooksESLintRule,
+    testsBabelEslint
+  );
+
+  const testsTypescriptEslintParser = testStaticHooks;
 
   new ESLintTesterV7({
     parser: require.resolve("@typescript-eslint/parser-v2"),
